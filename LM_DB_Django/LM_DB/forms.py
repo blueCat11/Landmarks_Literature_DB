aa3fb2f6f82d29b8e0c1db3a5df8c8f77f7248e5
Anni-Laptop <blue.cat@t-online.de>
2018-07-29 15:29:44 +0200

    forms for core attributes and links

--- LM_DB_Django/LM_DB/forms.py
+++ LM_DB_Django/LM_DB/forms.py
@@ -3,17 +3,38 @@ from django import forms
 
 from LM_DB.models import *
 
-
+# form for main information on paper
 class PaperForm (forms.ModelForm):
+    paper_id = forms.IntegerField(widget=forms.HiddenInput())
     doi = forms.CharField(max_length=50)
     bibtex = forms.CharField(widget=forms.Textarea, required=False)
     cite_command = forms.CharField(max_length=50, required=False)
     title = forms.CharField(widget=forms.Textarea, required=False)
     abstract = forms.CharField(widget=forms.Textarea, required=False)
     is_fulltext_in_repo = forms.BooleanField(required=False)
-    paper_id = forms.IntegerField(widget=forms.HiddenInput())
+
 
     class Meta:
         model = Papers
         fields = ('paper_id', 'doi', 'bibtex', 'cite_command', 'title', 'abstract')
 
+
+class CoreAttributeForm (forms.ModelForm):
+    core_attribute_id = forms.IntegerField(widget=forms.HiddenInput())
+    core_attribute = forms.CharField(widget=forms.Textarea, required=False)
+    is_literal_quotation = forms.BooleanField(required=False)
+    page_num = forms.IntegerField(required=False)
+
+    class Meta:
+        model = CoreAttributes
+        fields = ('core_attribute_id', 'core_attribute', 'is_literal_quotation', 'page_num')
+
+
+class LinkForm (forms.ModelForm):
+    link_id = forms.IntegerField(widget=forms.HiddenInput())
+    link_text = forms.CharField(widget=forms.Textarea, required=False)
+    is_local_link = forms.BooleanField(required=False)
+
+    class Meta:
+        model=Links
+        fields=('link_id', 'link_text', 'is_local_link')
